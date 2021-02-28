2921a57d60e46d34369b4fe96e46919e916c83fb
Trinadh Jagarlamudi <tjagarlamudi@workstation8.attlocal.net>
2021-02-28 00:24:08 -0600

    Adding springboot skeleton.

---
+++ src/main/java/com/tri909/testcal/TestCalApplication.java
@@ -0,0 +1,13 @@
+package com.tri909.testcal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCalApplication.class, args);
+	}
+
+}
